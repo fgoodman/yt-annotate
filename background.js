8aa866488a6c803888c74b7ff121f22e7961a158
Frank Goodman <frank_goodman@brown.edu>
2015-02-07 23:18:05 -0500

    dropping annotation bombs

--- background.js
+++ background.js
@@ -1,10 +1,28 @@
-function createAnnotation(time, totalTime) {
-	var percent = (time / totalTime) * 100;
+function createAnnotation(percent) {
+	percent = percent * 100;
+	console.log(percent);
 	var annotation = $("<div style=\"left: " + percent + "%;\" />");
 	annotation.addClass("ytp-annotation-marker");
 	$("#player-api .html5-progress-bar .ytp-progress-list").append(annotation);
 }
 
-for (var i = 0; i < 5 * 60 + 9; i += 60) {
-	createAnnotation(i, 5 * 60 + 9);
-}
+
+var span = $("<span id=\"add-annotation\" />");
+span.addClass("yt-uix-button-menu-item");
+span.addClass("html5-context-menu-copy-video-url");
+span.text("Add Annotation");
+
+var li = $("<li />");
+li.append(span);
+
+$(".html5-context-menu").prepend(li);
+
+var x = 0;
+$(document).bind("contextmenu", function (e) {
+	x = e.pageX;
+});
+
+$("#add-annotation").on("click", function () {
+	$("#body-container").click();
+	createAnnotation((x - $(".html5-video-player").offset().left) / $(".html5-video-player").width());
+});
