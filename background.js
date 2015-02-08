c374523f9eda310960fc2094da8bf5a8f84e00bd
Frank Goodman <frank_goodman@brown.edu>
2015-02-07 21:22:40 -0500

    tooltip markers

--- background.js
+++ background.js
@@ -1,2 +1,10 @@
-alert("Hello world");
-console.log("HELLO WORLD!");
+function createAnnotation(time, totalTime) {
+	var percent = (time / totalTime) * 100;
+	var annotation = $("<div style=\"left: " + percent + "%;\" />");
+	annotation.addClass("ytp-annotation-marker");
+	$("#player-api .html5-progress-bar .ytp-progress-list").append(annotation);
+}
+
+for (var i = 0; i < 5 * 60 + 9; i += 60) {
+	createAnnotation(i, 5 * 60 + 9);
+}
