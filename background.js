cd5485742f3115b108a8ff2c31ff456e8b40266d
Frank Goodman <frank_goodman@brown.edu>
2015-02-08 10:23:33 -0500

    adding and removing annotations

--- background.js
+++ background.js
@@ -1,20 +1,82 @@
+var annotations = [];
+
 function createAnnotation(percent) {
 	percent = percent * 100;
-	var annotation = $("<div style=\"left: " + percent + "%;\" />");
+	annotations.push(percent);
+	var annotation = $("<div id=\"a" + percent.toString().replace(/\./g, "").substring(0, 5) + "\" style=\"left: " + percent + "%;\" />");
+	console.log(percent.toString().replace(/\./g, "").substring(0, 5));
 	annotation.addClass("ytp-annotation-marker");
 	$("#player-api .html5-progress-bar .ytp-progress-list").append(annotation);
 }
 
+function removeAnnotation(percent) {
+	var as = annotations.map(function (v) {
+		return Math.abs(v - (100 * percent));
+	});
+	var m = Math.min.apply(Math, as);
+	console.log(m);
+	var i = as.indexOf(m);
+	var n = (annotations[i] * 100).toString().replace(/\./g, "");
+	console.log(n.substring(0, 5));
+	$("#a" + n.substring(0, 5)).remove();
+	annotations.splice(i, 1);
+}
 
-var span = $("<span id=\"add-annotation\" />");
-span.addClass("yt-uix-button-menu-item");
-span.addClass("html5-context-menu-copy-video-url");
-span.text("Add Annotation");
+var add = $("<span id=\"add-annotation\" style=\"display: none;\" />");
+add.addClass("yt-uix-button-menu-item");
+add.addClass("html5-context-menu-copy-video-url");
+add.text("Add Annotation");
+
+var remove = $("<span id=\"remove-annotation\" style=\"display: none;\" />");
+remove.addClass("yt-uix-button-menu-item");
+remove.addClass("html5-context-menu-copy-video-url");
+remove.text("Remove Annotation");
 
 var li = $("<li />");
-li.append(span);
+li.append(add);
+li.append(remove);
 
 $(".html5-context-menu").prepend(li);
+
+function show(what) {
+	$("#" + what + "-annotation").css("display", "block");
+}
+
+function hide(what) {
+	$("#" + what + "-annotation").css("display", "none");
+}
+
+$(".html5-progress-bar, .html5-context-menu").hover(function (e) {
+	var as = annotations.map(function (v) {
+		return Math.abs(v - (100 * (e.pageX - $(".html5-video-player").offset().left) / $(".html5-video-player").width()));
+	});
+	var y = false;
+	for (var i = 0; i < as.length; i++) {
+		if (as[i] < 1.0) {
+			y = true;
+			break;
+		}
+	}
+
+	if (y) {
+		hide("add");
+		show("remove");
+	}
+	else {
+		hide("remove");
+		show("add");
+	}
+}, function () {
+	if (!$(".html5-context-menu").is(":visible")) {
+		hide("add");
+		hide("remove");
+	}
+});
+
+$(".html5-context-menu").hide(0, function () {
+	hide("add");
+	hide("remove");
+});
 
 var x = 0;
 $(document).bind("contextmenu", function (e) {
@@ -24,4 +86,9 @@ $(document).bind("contextmenu", function (e) {
 $("#add-annotation").on("click", function () {
 	$("#body-container").click(); // hack
 	createAnnotation((x - $(".html5-video-player").offset().left) / $(".html5-video-player").width());
+});
+
+$("#remove-annotation").on("click", function () {
+	$("#body-container").click();
+	removeAnnotation((x - $(".html5-video-player").offset().left) / $(".html5-video-player").width());
 });
